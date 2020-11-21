c0ee8eaf50acab4ab73a7386555730fa3f5c426c
Philipp Brumm <philipp.brumm@gmail.com>
2020-11-21 15:10:54 +0100

    base playing field and states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'modern-normalize'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['red', 'green', 'blue', 'pink', 'yellow', 'gray']
+
+const makeRandomPattern = () =>
+  Array.from({ length: columns }, () => {
+    const colorIndex = Math.floor(Math.random() * colors.length)
+    return colors[colorIndex]
+  })
+
+const columns = 4
+const rows = 8
+
+const patternToBeGuessed = makeRandomPattern()
+
+const getResult = (patternToBeGuessed, playerPattern) => {
+  return playerPattern
+    .map((pattern, index) => {
+      if (patternToBeGuessed[index] === pattern) {
+        return 'black'
+      } else if (patternToBeGuessed.includes(pattern)) {
+        return 'white'
+      }
+    })
+    .filter(Boolean)
+    .sort()
 }
 
-export default App;
+export default function App() {
+  const visibleColumns = columns + 2
+  const visibleRows = rows + 1
+  const [guesses, setGuesses] = React.useState([makeRandomPattern()])
+  const results = guesses.map((guess) => getResult(patternToBeGuessed, guess))
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        width: '100vw',
+        height: '100vh',
+        gridTemplateColumns: `repeat(${visibleColumns}, 50px)`,
+        gridTemplateRows: `repeat(${visibleRows}, 50px)`,
+        gridGap: 5,
+      }}
+    >
+      <div
+        style={{
+          display: 'contents',
+        }}
+      >
+        <div></div>
+        <div></div>
+        {patternToBeGuessed.map((thing) => (
+          <div
+            style={{
+              backgroundColor: thing,
+            }}
+          ></div>
+        ))}
+      </div>
+
+      {Array.from({ length: rows }).map((_, rowIndex, { length }) => (
+        <div
+          style={{
+            display: 'contents',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {length - rowIndex}
+          </div>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(2, 1fr)`,
+              gridTemplateRows: `repeat(2, 1fr)`,
+              backgroundColor: 'gray',
+              gridGap: 5,
+              padding: 5,
+            }}
+          >
+            {results[length - rowIndex - 1] &&
+              results[length - rowIndex - 1].map((result) => (
+                <div
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: '50%',
+                    backgroundColor: result,
+                  }}
+                />
+              ))}
+          </div>
+
+          {Array.from({ length: columns }).map((_, columnIndex) => (
+            <div
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: '50%',
+                backgroundColor:
+                  guesses[length - rowIndex - 1] &&
+                  guesses[length - rowIndex - 1][columnIndex],
+              }}
+            ></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
