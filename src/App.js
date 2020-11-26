a836faa1d998325c337eda0dafff2b69a8d83848
JW1704 <julian.weusten@fondof.de>
2020-11-26 19:21:10 +0100

    generate custom cell in userinput

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React, { Fragment } from 'react'
-import 'modern-normalize'
+import React, { Fragment } from "react";
+import "modern-normalize";
 
-import { generatePattern, getResult, colors } from './utils'
+import { generatePattern, getResult, colors } from "./utils";
 
-const UserInput = ({ prop }) => {
+const SpecialCell = ({ clicked, children, amount }) => {
   return (
     <Fragment>
-      <div>foo</div>
-      <div>foo</div>
-      <div>foo</div>
-      <div>foo</div>
-      <div>foo</div>
+      {Array.from({ length: amount }).map((_, columnIndex) => {
+        return (
+          <div
+            style={{ display: "contents" }}
+            id={columnIndex}
+            key={columnIndex}
+            onClick={clicked}
+          >
+            {children}
+          </div>
+        );
+      })}
     </Fragment>
-  )
-}
+  );
+};
+
+const UserInput = ({ columns }) => {
+  return (
+    <Fragment>
+      <SpecialCell amount={columns} clicked={() => console.log("hello")}>
+        <div
+          style={{
+            borderRadius: "50%",
+            backgroundColor: "#faecd7",
+            textAlign: "center",
+          }}
+        >
+          foo
+        </div>
+      </SpecialCell>
+    </Fragment>
+  );
+};
 
 const Game = ({ columns, rows, secretPattern }) => {
-  const [guesses, setGuesses] = React.useState([])
-  const results = guesses.map((guess) => getResult(secretPattern, guess))
+  const [guesses, setGuesses] = React.useState([]);
+  const results = guesses.map((guess) => getResult(secretPattern, guess));
   const hasWon = results.some(
     (result) =>
-      result.length === columns && result.every((pin) => pin === 'black'),
-  )
+      result.length === columns && result.every((pin) => pin === "black")
+  );
 
   // if (hasWon) {
   //   return <div>you won! 🎉</div>
@@ -31,8 +56,8 @@ const Game = ({ columns, rows, secretPattern }) => {
     <Fragment>
       <div
         style={{
-          display: 'inline-grid',
-          '--slot-size': '100px',
+          display: "inline-grid",
+          "--slot-size": "100px",
           gridTemplateColumns: `var(--slot-size) repeat(${columns}, var(--slot-size)) var(--slot-size)`,
           gridTemplateRows: `var(--slot-size) repeat(${rows}, var(--slot-size)) var(--slot-size)`,
           gridGap: 10,
@@ -41,7 +66,7 @@ const Game = ({ columns, rows, secretPattern }) => {
       >
         <div
           style={{
-            display: 'contents',
+            display: "contents",
           }}
         >
           <div />
@@ -50,7 +75,7 @@ const Game = ({ columns, rows, secretPattern }) => {
               style={{
                 gridColumnStart: index + 2,
                 backgroundColor: colors[value],
-                borderRadius: '50%',
+                borderRadius: "50%",
               }}
             />
           ))}
@@ -60,85 +85,84 @@ const Game = ({ columns, rows, secretPattern }) => {
         {Array.from({ length: rows }).map((_, rowIndex, { length }) => (
           <div
             style={{
-              display: 'contents',
+              display: "contents",
             }}
           >
             <div
               style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                fontSize: 'calc(var(--slot-size) * 0.6)',
-                fontWeight: 'bold',
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "calc(var(--slot-size) * 0.6)",
+                fontWeight: "bold",
                 color:
-                  guesses.length === length - rowIndex - 1 ? 'red' : '#faecd7',
+                  guesses.length === length - rowIndex - 1 ? "red" : "#faecd7",
               }}
             >
               {length - rowIndex}
             </div>
 
             {guesses.length === length - rowIndex - 1 ? (
-              <UserInput />
+              <UserInput columns={columns} />
             ) : (
               <Fragment>
                 {Array.from({ length: columns }).map((_, columnIndex) => {
                   const value =
                     guesses[length - rowIndex - 1] &&
-                    guesses[length - rowIndex - 1][columnIndex]
+                    guesses[length - rowIndex - 1][columnIndex];
 
                   return (
                     <div
                       style={{
-                        borderRadius: '50%',
-                        backgroundColor: colors[value] || '#faecd7',
+                        borderRadius: "50%",
+                        backgroundColor: colors[value] || "#faecd7",
                       }}
                     ></div>
-                  )
+                  );
                 })}
-
-                <div
-                  style={{
-                    display: 'grid',
-                    gridTemplateColumns: `repeat(${columns / 2}, 1fr)`,
-                    gridTemplateRows: `repeat(${columns / 2}, 1fr)`,
-                    backgroundColor: '#faecd7',
-                    borderRadius: 5,
-                    gridGap: 7,
-                    padding: 7,
-                  }}
-                >
-                  {results[length - rowIndex - 1] &&
-                    results[length - rowIndex - 1].map((result) => (
-                      <div
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          borderRadius: '50%',
-                          backgroundColor: result,
-                        }}
-                      />
-                    ))}
-                </div>
               </Fragment>
             )}
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: `repeat(${columns / 2}, 1fr)`,
+                gridTemplateRows: `repeat(${columns / 2}, 1fr)`,
+                backgroundColor: "#faecd7",
+                borderRadius: 5,
+                gridGap: 7,
+                padding: 7,
+              }}
+            >
+              {results[length - rowIndex - 1] &&
+                results[length - rowIndex - 1].map((result) => (
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      borderRadius: "50%",
+                      backgroundColor: result,
+                    }}
+                  />
+                ))}
+            </div>
           </div>
         ))}
 
         <div />
         {colors.map((color) => (
-          <div style={{ backgroundColor: color, borderRadius: '50%' }} />
+          <div style={{ backgroundColor: color, borderRadius: "50%" }} />
         ))}
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
 const App = () => {
-  const columns = 4
+  const columns = 4;
 
   return (
     <Game columns={columns} rows={6} secretPattern={generatePattern(columns)} />
-  )
-}
+  );
+};
 
-export default App
+export default App;
